e6705548207280961adb00eacb1c9eb711b89db0
Sumit Kumar <sumit.kumar@pinelabs.com>
2021-06-16 19:25:32 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   env: {
-    DB_LOCAL_URI: '',
-    DB_URI: '',
+    DB_LOCAL_URI: process.env.DB_LOCAL_URI,
+    DB_URI: process.env.DB_URI,
 
-    STRIPE_API_KEY: '',
-    STRIPE_SECRET_KEY: '',
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
 
-    STRIPE_WEBHOOK_SECRET: '',
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
 
-    CLOUDINARY_CLOUD_NAME: '',
-    CLOUDINARY_API_KEY: '',
-    CLOUDINARY_API_SECRET: '',
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 
-    SMTP_HOST: '',
-    SMTP_PORT: '',
-    SMTP_USER: '',
-    SMTP_PASSWORD: '',
-    SMTP_FROM_EMAIL: '',
-    SMTP_FROM_NAME: '',
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+    SMTP_FROM_NAME: process.env.SMTP_FROM_NAME,
 
-    NEXTAUTH_URL: '',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
   images: {
     domains: ['res.cloudinary.com'],
